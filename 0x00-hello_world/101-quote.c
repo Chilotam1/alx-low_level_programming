7f7f4a47b643eed7d804b3db404d5ead476b830b
Chilotam1 <chilotamajoku@gmail.com>
2023-04-21 06:43:16 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry Point
+ * main - A program that prints a line to the standard error
  * Return: 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 61");
 return (1);
 }
