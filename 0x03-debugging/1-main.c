44669b70f97b0b9069f5567acf69bc923a3c56b3
Chilotam1 <chilotamajoku@gmail.com>
2023-04-28 12:19:50 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,10 +5,7 @@
 * Return: 0
 */
 
-
-
 int main(void)
-
 {
 	int i;
 
@@ -16,7 +13,7 @@ int main(void)
 	i = 0;
 	/*while (i < 10)*/
 	/*{*/
-	/*	putchar(i);*/
+	/*         putchar(i);*/
 	/*}*/
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
