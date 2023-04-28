ce30fad27d120a88e64a9a8453517ffd2fc89ad5
Chilotam1 <chilotamajoku@gmail.com>
2023-04-28 11:12:32 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,11 +5,14 @@
 * Return: 0
 */
 
-int main(void)
-{
-        int i;
 
-        printf("Infinite loop incoming :(\n");
+int main(void)
+
+{
+        
+	int i;
+	
+	printf("Infinite loop incoming :(\n");
 	i = 0;
 	/*while (i < 10)*/
         /*{*/
