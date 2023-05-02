cf61940b09073470dded4d02c950a2ff5fa6746b
Chilotam1 <chilotamajoku@gmail.com>
2023-05-02 11:32:42 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,15 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int j = 0;
 
-	while (str[i] != '\0')
+	while (str[j] != '\0')
 	{
-	i++;
+	j++;
+	}
+	for (i = 0; i < j; i += 2)
+	{
 	putchar(str[i]);
-	i += 2;
 	}
 	putchar('\n');
 }
