f3e2602161bdd2106bf573999b9120c006202fab
Chilotam1 <chilotamajoku@gmail.com>
2023-05-02 09:00:18 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,14 +4,14 @@
  * @str: string
  * Return: length
  */
-size_t _strlen(char *s)
+size_t _strlen(const char *str)
 {
 	size_t length = 0;
 
-	while (*s != '\0')
+	while (*str != '\0')
 	{
 	length++;
-	s++;
+	str++;
 	}
 	return (length);
 }
