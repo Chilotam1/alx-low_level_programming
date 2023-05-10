177b634ca509c0ae9c4ae812d217696d904f8d33
Chilotam1 <chilotamajoku@gmail.com>
2023-05-10 14:20:57 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,13 +3,12 @@
 
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
 int _putchar(char c);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _sqrt(int n, int i);
+int is_prime_number(int n);
 
 #endif
