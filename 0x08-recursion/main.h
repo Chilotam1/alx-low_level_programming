4e41b538be2e5a2bc3ed1120f8922508e531d0ee
Chilotam1 <chilotamajoku@gmail.com>
2023-05-10 09:25:23 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
+#endif
